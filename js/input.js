1a71f1ba8c0560f45b760a6b2b231be4bfb385fa
Evgen Volontyr <soapforwork@gmail.com>
2016-08-20 13:41:23 +0300

    v2.0

--- js/input.js
+++ js/input.js
@@ -7,126 +7,36 @@ class Input /*extends Calc*/ {
     // super();
     this._el = options.el;
 
-    this._number = '';
-    this._symbolAcc = '';
-    this._numberAcc = 0;
-    this._total = 0;
+    this._operators = ['+', '-', 'x', '÷'];
+    this._decimalAdded = false;
+
 
     let _self = this;
     this._el.addEventListener('click', this._putInput.bind(_self));
   }
 
-  _inputOption(option) {
-    switch (option) {
-      case 'reset':
-      calc._output._clearOutput('reset', true, true);
-      break;
-    }
-  }
-
   _putInput(e) {
     let target = e.target;
+    console.log(target.dataset);
 
     if (target.dataset.number) {
-      this._inputNumber(target.dataset.number);
+      console.log(target.dataset.number);
+      // calc._output.getElement().innerHTML += target.dataset.number;
 
     } else if (target.dataset.symbol) {
-      if (target.dataset.symbol === 'dote') {
-        return this._inputNumber('.');
-      }
-      this._inputSymbol(target.dataset.symbol);
+      buttonValue = target.dataset.symbol;
+      outputValue += buttonValue;
 
     } else if (target.dataset.option) {
-      this._inputOption(target.dataset.option);
+      buttonValue = target.dataset.option;
+
+      switch (buttonValue) {
+        case 'reset':
+          outputValue = '';
+          break;
+      }
 
     }
-  }
-
-  _inputNumber(number) {
-    this._number += number;
-    this._toOutput(this._number); /// ???
-  }
-
-  _inputSymbol(symbol) {
-    console.log(
-      symbol,
-      this._number,
-      this._symbolAcc,
-      this._numberAcc,
-      this._total
-    );
-
-    this._symbolAcc = symbol;
-    this._numberAcc = this._number;
-    this._number = '';
-
-    // if (!this._symbolAcc) {
-    //
-    //   switch (symbol) {
-    //     case 'division':
-    //     case 'multiply':
-    //     case 'minus':
-    //     case 'plus':
-    //       return;
-    //   }
-    // }
-
-    this._inputToTotal(symbol);
-  }
-
-  _inputToTotal(operator) {
-
-      console.log(
-        operator,
-        this._number,
-        this._symbolAcc,
-        this._numberAcc,
-        this._total
-      );
-
-    switch (this._symbolAcc) {
-      case 'division':
-        this._total = Number(this._numberAcc) / Number(this._number);
-        break;
-      case 'multiply':
-        this._total = Number(this._numberAcc) * Number(this._number);
-        break;
-      case 'minus':
-        this._total = Number(this._numberAcc) - Number(this._number);
-        break;
-      case 'plus':
-        this._total = Number(this._numberAcc) + Number(this._number);
-        break;
-      case 'square':
-        this._total = Math.sqrt(Number(this._numberAcc));
-        break;
-    }
-
-    this._numberAcc = this._total;
-
-    if (operator === 'equally') {
-      this._toOutput(this._total, false, true); /// ???
-      // this._numberAcc = this._total;
-    }
-
-
-      console.log(
-        operator,
-        this._number,
-        this._symbolAcc,
-        this._numberAcc,
-        this._total
-      );
-  }
-
-  _toOutput(value) { /// ???
-    if (value % 1 === 0) {
-      value += '.';
-    } else {
-      value += '\u0020';
-    }
-
-    calc._output._clearOutput(value);
   }
 
   getElement() {
